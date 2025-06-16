e72516220cdf74d852a4cef2969c60b600c89790
Ervey Garcia <ervey.garcia@carrier.com>
2025-06-16 13:52:26 -0600

    fix: avoid undefined access in cleanDb.ts

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,11 +3,17 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
 
-    if (modelExists.length) {
+    if (!model?.db?.db) {
+      throw new Error(`Model ${modelName} does not have a valid db connection`);
+    }
+
+    const collections = await model.db.db.listCollections({
+      name: collectionName
+    }).toArray();
+
+    if (collections.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
