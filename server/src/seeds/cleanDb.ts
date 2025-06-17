83ecd23b0b3973e496ae7d713d6b36497be17d60
Ervey Garcia <ervey.garcia@carrier.com>
2025-06-16 19:28:18 -0600

    fix: fully handle undefined db references in cleanDb

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,20 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
 
-    if (modelExists.length) {
+    if (!model || !model.db || !model.db.db) {
+      throw new Error(`Missing db connection in model "${modelName}"`);
+    }
+
+    const collections = await model.db.db.listCollections({
+      name: collectionName
+    }).toArray();
+
+    if (collections.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
-}
+};
